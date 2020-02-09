eb22a078ecff8fffdbecca36e1174d9f0ef6bff9
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-02-09 14:07:12 +0900

    Add admin CSV

---
+++ app/controllers/admin/restaurants_controller.rb
@@ -0,0 +1,8 @@
+class Admin::RestaurantsController < ApplicationController
+  before_action :if_not_admin
+
+  private
+  def if_not_admin
+    redirect_to posts_path unless current_user.admin?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :restaurants, only: [:index, :show] 
+  namespace :admin do
+    resources :restaurants, only: [:index, :new, :create, :show,  :edit, :destroy]
+  end
   devise_for :users
   root 'welcome#index'
   resources :manufacturers
