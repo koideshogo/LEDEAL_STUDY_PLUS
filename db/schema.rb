f297415aae4a0f235a4b6ff04ad6080e960e024e
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-02-09 13:32:38 +0900

    Add rails_admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_142521) do
+ActiveRecord::Schema.define(version: 2020_02_09_041555) do
 
   create_table "manufacturers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_02_05_142521) do
     t.datetime "updated_at", null: false
     t.text "name"
     t.integer "staff_num"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
