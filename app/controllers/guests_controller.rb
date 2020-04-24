5ae29778880e31f7d1ba22e7236be05bc226747c
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-04-24 16:36:17 +0900

    CreateGuestController

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,4 @@
+class GuestsController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'guests/create'
   get 'categories/index'
   root 'mains#index'
   get 'mains/index'
