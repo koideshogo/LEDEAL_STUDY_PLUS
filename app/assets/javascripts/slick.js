aba8c8774000fe1de80b4349714ef95fc8ada00f
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-04-26 11:55:19 +0900

    AddTopPageInfomation

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -1,7 +1,7 @@
 $(function(){
-  $('.main-slider').slick({
+  $('.hero__slider').slick({
     autoplay:true,
-    autoplaySpeed:5000,
+    autoplaySpeed:8000,
     dots:false,
     prevArrow: '<span class="prev-arrow">&lt;</span>',
     nextArrow: '<span class="next-arrow">&gt;</span>'
