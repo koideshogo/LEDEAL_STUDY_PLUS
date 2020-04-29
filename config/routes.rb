9b63be291948766f76f8e6e81aced71a05a1ad36
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-04-29 19:29:03 +0900

    searchルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,21 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  
   # 管理者権限path
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :restaurants, only: %i[index show]
   namespace :admin do
     resources :restaurants, only: %i[index new create show edit destroy]
   end
+
   # manufactuerのパス
   resources :manufacturers, only: %i[index new create edit destroy]
   get 'manufacturers/:id', to: 'manufacturers#show', as: 'manufacturer_show'
+
   # postのパス
   resources :posts
+  get 'search', to: 'posts#search', as: 'search'
   namespace :api, format: 'json' do
     # categoryセレクトボックスで使用
     resources :categories, only: [:index]
