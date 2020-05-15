62b9a4b08da1c4872442844b463b69b15539c0d6
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-05-15 17:49:53 +0900

    use_idとpost_idのユニーク制約

--- app/models/output.rb
+++ app/models/output.rb
@@ -1,6 +1,7 @@
 class Output < ApplicationRecord
   validates :body, :salon_name, :user_id, :post_id, presence: true
-  validates :user_id && :post_id, uniqueness: true
+  validates :user_id, uniqueness: { scope: [:post_id] }
+
   belongs_to :user
   belongs_to :post
 end
