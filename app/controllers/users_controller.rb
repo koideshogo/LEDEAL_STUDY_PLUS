1a297e0658759fd71ed01773f736af2e37ff1949
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-05-16 23:41:57 +0900

    マイページにアウトプット内容表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,11 @@ class UsersController < ApplicationController
   before_action :set_categories, only: %i[show]
   def show
     @user = User.find(current_user.id)
-    @out_put = Output.where(user_id: current_user.id)
-    @post_id = Output.find_by(user_id: current_user.id)
-    @post = Post.where(id: "#{@post_id&.post_id}")
+    @out_puts = Output.where(user_id: current_user.id)
+    @out_puts.each do |out_put|
+    @posts = Post.where("id= #{out_put.post_id}")
+
+    end
   end
 
   private
