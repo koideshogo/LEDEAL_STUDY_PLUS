e091f3c0304c34155dbce9b6fe3675f6824c53ed
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-06-02 22:10:12 +0900

    AddUser_Rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "新規登録" do
+    it "name,staff_num,mail,passがある状態だと新規登録ができる" do
+      user = FactoryBot.create(:user)
+      expect(user).to be_valid
+    end
+
+    it 'nameがない場合新規登録ができない' do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
+    it "staff_numがない場合新規登録ができない" do
+      user = build(:user, staff_num: nil)
+      user.valid?
+      expect(user.errors[:staff_num]).to include("を入力してください")
+    end
+    it "mailがない場合新規登録ができない" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+    it "重複したstaff_numがある場合新規登録ができない" do
+      user = create(:user)
+      another_user = build(:user, staff_num: user.staff_num)
+      another_user.valid?
+      expect(another_user.errors[:staff_num]).to include("はすでに存在します")
+    end
+    it "パスワードがない場合新規登録ができない" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+    it "重複したメールアドレスがある場合新規登録ができない" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
+  end
 end
