97c9b9d0e7778446170742bb41f50ed326f3925f
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-06-03 15:00:37 +0900

    Add_PostModel_Validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
-  validates :body, :youtube_url, :title, :release_date, presence: true
+  validates :body, :youtube_url, :title, :release_date, :category_id,
+            :category1, :category2, presence: true
   belongs_to :user
   belongs_to :category
   has_many :likes, dependent: :destroy
   has_many :outputs, dependent: :destroy
+
 
   def like_user(user_id)
     likes.find_by(user_id: user_id)
