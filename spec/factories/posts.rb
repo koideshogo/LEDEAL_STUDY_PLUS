916157c0413761ddece30db7b2e59b5e3c1edbfd
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-06-03 14:59:43 +0900

    AddFactoryBot_Post

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :post do
-    
+    body {"hogehogehogehogehogehohgoge"}
+    youtube_url {"GqG3eqy2QqU"}
+    title {"hogehoge"}
+    release_date {"2020-04-04"}
+    category
+    category1 { 1 }
+    category2 { 10 }
+    user
+    likes_count nil
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "動画投稿" do
+    it "body,youtube_url,title,release_dataが全てなければ投稿できない" do
+      post = build(:post)
+      expect(post).to be_valid
+    end
+  end
 end
