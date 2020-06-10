a24cc33d164d40fe800e511b98467bb6ece3beab
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-06-10 23:40:25 +0900

    コメントアウト削除

--- app/assets/javascripts/top-page-btn.js
+++ app/assets/javascripts/top-page-btn.js
@@ -1,30 +1,3 @@
-// // スクロールして何ピクセルでアニメーションさせるか
-// var px_change = 1;
-// // スクロールのイベントハンドラを登録
-// window.addEventListener('scroll', function(e) {
-//   console.log(e);
-//     // 変化するポイントまでスクロールしたらクラスを追加
-//     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//     if ( scrollTop > px_change ) {
-//         document.getElementById( "btn-backtotop" ).classList.add( "fadein" );
-
-//     // 変化するポイント以前であればクラスを削除
-//     } else {
-//         document.getElementById( "btn-backtotop" ).classList.remove( "fadein" );
-//     }
-// });
-
-// document.getElementById( "btn-backtotop" ).addEventListener('click', function(e) {
-//     anime.remove("html, body");
-//     anime({
-//         targets: "html, body",
-//         scrollTop: 0,
-//         dulation: 600,
-//         easing: 'easeOutCubic',
-//     });
-//     return false;
-// });
-
 $(function () {
     var topBtn = $('#page-top');
     topBtn.hide();
