621296a839fc6dbad10e07b65652280e536d947c
koideshogo <56552221+koideshogo@users.noreply.github.com>
2020-07-09 11:12:43 +0900

    DockerSetting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # bettererror
   BetterErrors::Middleware.allow_ip! '0.0.0.0/0'
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
 end
